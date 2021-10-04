381aa61ca3fd9791c997606193b12ef2d9074489
LaurieCLu <59483905+LaurieClu@users.noreply.github.com>
2021-10-04 16:27:24 +0200

    Update wall.php

--- src/htdocs/resoc_n1/pages/wall.php
+++ src/htdocs/resoc_n1/pages/wall.php
@@ -51,13 +51,13 @@
                 $laQuestionEnSql = "SELECT * FROM `users` WHERE id=" . intval($userId);
                 $lesInformations = $mysqli->query($laQuestionEnSql);
                 $user = $lesInformations->fetch_assoc();
-                //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par l'alias et effacer la ligne ci-dessous
+               
                 echo "<pre>" . print_r($user, 1) . "</pre>";
                 ?>
                 <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
                 <section>
                     <h3>Présentation</h3>
-                    <p>Sur cette page vous trouverez tous les message de l'utilisatrice : XXX
+                    <p>Sur cette page vous trouverez tous les message de l'utilisatrice : <?php echo print_r($user['alias'],50)?>
                         (n° <?php echo $_GET['user_id'] ?>)
                     </p>
                 </section>
@@ -87,9 +87,6 @@
                     echo("Échec de la requete : " . $mysqli->error);
                 }
 
-                /**
-                 * Etape 4: @todo Parcourir les messsages et remplir correctement le HTML avec les bonnes valeurs php
-                 */
                 while ($post = $lesInformations->fetch_assoc())
                 {
 
@@ -97,20 +94,15 @@
                     ?>                
                     <article>
                         <h3>
-                            <time datetime='2020-02-01 11:12:13' >31 février 2010 à 11h12</time>
+                            <time ><?php echo $post['created']?></time>
                         </h3>
-                        <address>par AreTirer</address>
+                        <address><?php echo $post['author_name']?></address>
                         <div>
-                            <p>Ceci est un paragraphe</p>
-                            <p>Ceci est un autre paragraphe</p>
-                            <p>... de toutes manières il faut supprimer cet 
-                                article et le remplacer par des informations en 
-                                provenance de la base de donnée</p>
+                            <p><?php echo $post['content']?></p>
                         </div>                                            
                         <footer>
-                            <small>♥ 132</small>
-                            <a href="">#lorem</a>,
-                            <a href="">#piscitur</a>,
+                            <small>♥ <?php echo $post['like_number']?></small>
+                            <a href="">#<?php echo $post['taglist']?></a>,
                         </footer>
                     </article>
                 <?php } ?>
